87b896957cb2941311151812fe998adb00359ae9
Luis Munhoz <luismunhoz@gmail.com>
2019-04-01 10:47:48 -0300

    Initial commit

---
+++ src/main/java/br/com/luismunhoz/CompareFilesApplication.java
@@ -0,0 +1,13 @@
+package br.com.luismunhoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompareFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompareFilesApplication.class, args);
+	}
+
+}
